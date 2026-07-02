17d9128cadcadfe7339db2968e2e4f092f39129b
LORPerfume <lorjordan4@gmail.com>
2026-07-02 14:18:17 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -210,10 +210,7 @@ def first_setup(request):
         elif len(password1) < 6:
             error = 'كلمة المرور يجب أن تكون 6 أحرف على الأقل'
         else:
-            user = User.objects.create_superuser(
-                username=username,
-                password=password1
-            )
+            user = User.objects.create_superuser(username=username, password=password1)
             login(request, user)
             return redirect('dashboard')
 
